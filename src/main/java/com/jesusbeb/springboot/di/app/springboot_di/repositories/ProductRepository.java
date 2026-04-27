02ebfcba54899e2395fe90e5494dd122d4f60776
jesusbeb <jesusbelb@gmail.com>
2026-04-27 13:27:38 -0600

    Creando la clase Model y la clase Repository

---
+++ src/main/java/com/jesusbeb/springboot/di/app/springboot_di/repositories/ProductRepository.java
@@ -0,0 +1,35 @@
+package com.jesusbeb.springboot.di.app.springboot_di.repositories;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.jesusbeb.springboot.di.app.springboot_di.models.Product;
+
+public class ProductRepository {
+
+    List<Product> data;
+
+    // Constructor. En este caso, se simula una base de datos con una lista de productos.
+    public ProductRepository() {
+        this.data = Arrays.asList(
+            new Product(1L, "Memoria corsair 32", 300L),
+            new Product(2L, "Cpu Intel Core i9", 850L),
+            new Product(3L, "Teclado Razer Mini 60%", 180L),
+            new Product(4L, "Motherboard Gigabyte", 490L)
+        );
+    }
+
+    // Método para obtener todos los productos
+    public List<Product> findAll() {
+        return this.data;
+    }
+
+    public Product findById(Long id) {
+        return data.stream()
+            .filter(p -> p.getId().equals(id))
+            .findFirst()
+            .orElse(null);
+    }
+
+
+}
